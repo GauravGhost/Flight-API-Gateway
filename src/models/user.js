b2f9e5dba204d630707895237f9cc2aacec29d16
gauravghost <gyanendrak874@gmail.com>
2023-06-04 00:19:57 +0530

    Adeed encryption in password

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,10 @@
 const {
   Model
 } = require('sequelize');
+
+const bcrypt = require('bcrypt');
+const {ServerConfig} = require('../config');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -33,5 +37,12 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+  
+  User.beforeCreate(async function encrypt(user){
+    const encryptedPassword = await bcrypt.hashSync(user.password, +ServerConfig.SALT_ROUNDS);
+    user.password = encryptedPassword;
+  })
+
+
   return User;
 };
