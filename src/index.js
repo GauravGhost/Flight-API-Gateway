211d24df9760ba18ba87542acd148702e12a52ca
gauravghost <gyanendrak874@gmail.com>
2023-06-03 20:27:18 +0530

    Added basic setup for flight api gateway

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const { ServerConfig, Logger } = require('./config');
+const apiRoutes = require('./routes')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, ()=>{
+    console.log(`Server has started in ${ServerConfig.PORT}`);
+    Logger.info("successfully started the server", "root", {})
+})
