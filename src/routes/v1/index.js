e191ba4b8556acc785063b8ad07a6f61155d96d4
gauravghost <gyanendrak874@gmail.com>
2023-06-04 02:26:47 +0530

    Added isAuthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const {InfoController} = require('../../controllers');
+const {AuthRequestMiddleware} = require('../../middleware');
 const userRouter = require('./user-routes');
 
 const router = express.Router();
 
-router.get('/info', InfoController.info);
+router.get('/info', AuthRequestMiddleware.checkAuth, InfoController.info);
 router.use('/user', userRouter);
 
 module.exports = router;
--- src/utils/common/auth.js
+++ src/utils/common/auth.js
@@ -21,8 +21,18 @@ function createToken(input){
     }
 }
 
+function verifyToken(token){
+    try {
+        return jwt.verify(token, ServerConfig.JWT_SECRET);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 
 module.exports = {
     checkPassword,
-    createToken
+    createToken,
+    verifyToken
 }
