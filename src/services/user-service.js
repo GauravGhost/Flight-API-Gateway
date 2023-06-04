2fc57c208631bfa3ed7010cdd2fddb9c4714b022
gauravghost <gyanendrak874@gmail.com>
2023-06-05 02:09:27 +0530

    Added default customer role while signup

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,13 +1,16 @@
 const { StatusCodes } = require('http-status-codes')
 const AppError = require('../utils/errors/app-error');
-const { UserRepository } = require('../repositories')
-const { Auth } = require('../utils/common');
+const { UserRepository, RoleRepository } = require('../repositories')
+const { Auth, Enums } = require('../utils/common');
 
 const userRepository = new UserRepository();
+const roleRepository = new RoleRepository();
 
 async function create(data) {
     try {
         const user = await userRepository.create(data);
+        const role = await roleRepository.getRoleByName(Enums.USER_ROLES_ENUMS.CUSTOMER);
+        user.addRole(role);
         return user;
     } catch (error) {
 
